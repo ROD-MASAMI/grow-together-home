f7850fc04938563315efa6f4225b9d36f79e6761
rod-masami <rodneypaul35@gmail.com>
2025-06-27 18:37:25 +0300

    nav bar name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         <div className="flex justify-between h-16">
           <div className="flex items-center">
             <Link to="/" className="flex-shrink-0 flex items-center">
-              <span className="text-white font-bold text-2xl drop-shadow-lg">GreenAcres</span>
+              <span className="text-white font-bold text-2xl drop-shadow-lg">Kingsliqour</span>
             </Link>
           </div>
           
